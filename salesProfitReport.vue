1dd5723b3af66e63dcfb0f571c4f06b8efb89d36
Sebastián Grignoli <grignoli@gmail.com>
2019-01-31 17:19:35 -0300

    Initial commit

---
+++ salesProfitReport.vue
@@ -0,0 +1,16 @@
+<template>
+    <a :href="href" v-html="text">XXX</a>
+</template>
+<script>
+export default {
+    props: ['href', 'text'],
+    data: function() {
+        return {
+
+        }
+    },
+    methods: {
+
+    }
+}
+</script>
