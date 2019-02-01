a7124702f4ca0df8c5efe3968291d691a7bf1941
Sebastián Grignoli <grignoli@gmail.com>
2019-01-31 21:42:40 -0300

    Component within component

---
+++ src/paginator.vue
@@ -0,0 +1,53 @@
+<template>
+        <div>[ THIS IS THE PAGINATOR ]</div>
+</template>
+<script>
+export default {
+    props: {
+        page: {
+            type: [Number, String],
+            default: 1
+        },
+        pages: {
+            type: [Number, String],
+            default: 1
+        },
+        links: {
+            type: [Number, String],
+            default: 5
+        },
+        next: {
+            type: [Boolean, String],
+            default: false
+        },
+        prev: {
+            type: [Boolean, String],
+            default: false
+        },
+        first: {
+            type: [Boolean, String],
+            default: false
+        },
+        last: {
+            type: [Boolean, String],
+            default: false
+        },
+    },
+    data: function() {
+        return {
+        }
+    },
+    methods: {
+        buttonsBefore() {
+            ;
+        },
+        buttonsAfter() {
+            ;
+        }
+    },
+    mounted() {
+    },
+}
+</script>
+<style>
+</style>
